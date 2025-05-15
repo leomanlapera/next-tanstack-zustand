e5ebfac63e5ef9a09cfbefcb0bc82057cd74ba0e
June Leoman Lapera <leomanlapera@gmail.com>
2025-05-15 19:27:04 +0800

    day 10

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,16 @@ export default function Home() {
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
       <div>
-        <div>daily practice apps</div>
+        <div className="mb-4 font-bold">daily practice apps</div>
         <ul className="list-inside list-disc">
           <li>
             <Link href="/todo" className="underline underline-offset-4">
               todo app
+            </Link>
+          </li>
+          <li>
+            <Link href="/tasks" className="underline underline-offset-4">
+              task management app
             </Link>
           </li>
         </ul>
